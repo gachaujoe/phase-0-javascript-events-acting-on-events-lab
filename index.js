454196886c1a19a9b09602e84f9d631419ff0d8c
Joee <gachaujoe@gmail.com>
2024-07-16 20:33:13 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+// index.js
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = dodger.style.left.replace('px', '');
+    left = parseInt(left, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = dodger.style.left.replace('px', '');
+    left = parseInt(left, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
